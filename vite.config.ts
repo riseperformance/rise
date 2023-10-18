d6516fa11a4a4c7c4903d338ae53890845f1c625
Gabriel Felix HUerta Araujo <gabrielwuerta@gmail.com>
2023-10-17 21:44:09 -0300

    add sitemap generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Pages from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rise/',
-  plugins: [react()],
-})
+  base: "/rise/",
+  plugins: [
+    react(),
+    Pages({
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+  ],
+});
