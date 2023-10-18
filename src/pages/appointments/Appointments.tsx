fd283a830e1a47a8cb6b2e9c189f6e54a2771210
Gabriel Felix HUerta Araujo <gabrielwuerta@gmail.com>
2023-10-17 21:38:15 -0300

    finish telinha

--- src/pages/appointments/Appointments.tsx
+++ src/pages/appointments/Appointments.tsx
@@ -1,7 +1,107 @@
+"use client";
+
+import { Button } from "@mui/material";
+import { PlanRefund } from "../home/components/plan-refund/PlanRefund";
 import styles from "./Appointments.module.scss";
+import { useEffect, useState } from "react";
+import blurDiamondVariant from "/blur_diamond_variant.svg";
+import about02 from "/images/about_02.png";
 
 export const Appointments = () => {
-  return <section className={styles.appointmentsSection}>
-    <h2>Teste</h2>
-  </section>;
+  const [banneStyle, setBannerStyle] = useState<React.CSSProperties>({});
+
+  const calculateBannerStyle = (): void => {
+    const computedFontSize = document.body
+      .computedStyleMap()
+      .get("font-size") as CSSUnitValue;
+
+    const fontSize = computedFontSize.value;
+    const scrollbarWidth = 0.5;
+    const width = document.body.clientWidth / fontSize - scrollbarWidth;
+    const widthPadding =
+      document.body.clientWidth > 1157
+        ? 5
+        : document.body.clientWidth > 930
+        ? 4
+        : document.body.clientWidth > 614
+        ? 3
+        : 2;
+    const bannerWidth = 776 / fontSize;
+
+    if (width <= 90) {
+      return setBannerStyle({
+        width: bannerWidth > width ? `${width}rem` : `${bannerWidth}rem`,
+        transform: `translateX(-${widthPadding}rem)`,
+        paddingLeft: `${widthPadding}rem`,
+      });
+    }
+
+    const leftGap = (width - 90) / 2;
+    setBannerStyle({
+      width: `${bannerWidth + leftGap}rem`,
+      transform: `translateX(-${leftGap + widthPadding}rem)`,
+      paddingLeft: `${leftGap + widthPadding}rem`,
+    });
+  };
+
+  useEffect(() => {
+    calculateBannerStyle();
+    window.addEventListener("resize", calculateBannerStyle);
+    return () => window.removeEventListener("resize", calculateBannerStyle);
+  }, []);
+
+  return (
+    <main className={styles.mainContainer}>
+      <div className={styles.titleContainer}>
+        <h1>Nossa consulta</h1>
+        <img
+          className={styles.diamondBlurImage}
+          src={blurDiamondVariant}
+          alt=""
+          draggable={false}
+        />
+      </div>
+
+      <section className={styles.aboutSection}>
+        <div className={styles.aboutContainer}>
+          <h2 data-aos="fade-right">Dr. André Scardovelli</h2>
+          <span data-aos="fade-right">Especialista em performance esportiva.</span>
+
+          <p data-aos="fade-right">
+            Sou médico com expertise na área da medicina desportiva e atuo com
+            grande enfoque no acompanhamento de atletas profissionais e amadores
+            que buscam melhora da condição física e performance.
+          </p>
+          <p data-aos="fade-right">
+            Fiz especialização em duas instituições, BCMED-(RS) e CEITT(SP) e
+            atualmente tenho me destacado pelos atendimentos na Clínica Rise.
+          </p>
+        </div>
+
+        <img src={about02} alt="André" />
+      </section>
+
+      <section className={styles.howItWorksSection}>
+        <h2 style={banneStyle}>Como funciona a consulta?</h2>
+
+        <p data-aos="fade-right">
+          A consulta tem duração de 1 hora, e envolve uma conversa aberta sobre
+          seus objetivos, necessidades, dúvidas e todo seu histórico de saúde,
+          esportivo e possíveis lesões.
+        </p>
+
+        <Button
+          variant="contained"
+          color="purple"
+          className={styles.scheduleExamButton}
+        >
+          Quero agendar minha consulta!
+        </Button>
+      </section>
+
+      <div className={styles.divider} />
+
+      <PlanRefund />
+    </main>
+  );
 };
