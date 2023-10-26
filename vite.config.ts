604591a8455d1ce9c8af6daa21a02eb92918a6c9
Gabriel Felix HUerta Araujo <gabrielwuerta@gmail.com>
2023-10-26 15:01:48 -0300

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
 import generateSitemap from "vite-plugin-pages-sitemap";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,6 @@ export default defineConfig({
         });
       },
     }),
-    ,
+    VitePWA(),
   ],
 });
